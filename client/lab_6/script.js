5de8c6dedfdef89d9a81b5d5587a729b6b65b677
VishnuRaja1 <vishnuraja@hotmail.com>
2021-10-09 22:11:54 -0400

    merge

---
+++ client/lab_6/script.js
@@ -0,0 +1,31 @@
+async function windowActions {
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const foodData = [];
+fetch(endpoint).then((blob) => blob.json()).then((data) => foodData.push(...data));
+
+function findMatches(wordsToMatch, foodData) {
+  return foodData.filter((place) => {
+    const regex = new RegExp(wordsToMatch, 'gi');
+    return place.name.match(regex) || place.city.match(regex);
+  });
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, foodData);
+  const html = matchArray.map((place) => `
+      <li>
+       <span class = "name">${place.name}, ${place.city}</span>
+      </li>
+      `).join('');
+  suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener('change', displayMatches);
+
+}
+
+window.onload = windowActions;
+
